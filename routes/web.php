1746fc99a5fe5ec678b6ce0a5782f27bc226511d
Miguel Rueda <msrr_2013@outlook.com>
2025-09-09 14:32:27 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,12 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
-
-Route::get('/products/create', [ProductController::class, 'create']);
-
-Route::get('/products/{id}/{category?}', [ProductController::class, 'detail']);
-
-
-
+Route::prefix('products')->controller(ProductController::class)->group(function(){
+    Route::get('/', 'index');
+    Route::get('/create', 'create');
+    Route::get('/{id}/{category?}', 'detail');
+});
